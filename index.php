2085445e8917b7a0f2424c52b327b94c87afbb4a
Noburou TANIGUCHI <dev@nota.m001.jp>
2015-09-26 17:45:30 +0900

    add ja and some debut output

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
+echo mb_internal_encoding() . '<br/>';
+mb_internal_encoding('UTF-8');
+echo mb_internal_encoding() . '<br/>';
+
 // I18N support information here
 $language = 'en_US';
+
+putenv('LANG=' . $language);
+setlocale(LC_ALL, $language);
+
+// Set the text domain as 'messages'
+$domain = 'messages';
+$localedir = dirname(__FILE__) . '/locale';
+echo $localedir . ': ';
+bindtextdomain($domain, $localedir);
+textdomain($domain);
+
+echo gettext('hello-world') . '<br/>';
+
+//////////
+
+$language = 'ja_JP';
 
 putenv('LANG=' . $language);
 setlocale(LC_ALL, $language);
