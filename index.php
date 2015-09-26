1718a6c160065ccac3f5aa9b4e188c75598393b4
Noburou TANIGUCHI <dev@nota.m001.jp>
2015-09-26 17:45:30 +0900

    Very simple gettext example

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// I18N support information here
+$language = 'en_US';
+
+putenv('LANG=' . $language);
+setlocale(LC_ALL, $language);
+
+// Set the text domain as 'messages'
+$domain = 'messages';
+$localedir = dirname(__FILE__) . '/locale';
+echo $localedir . ': ';
+bindtextdomain($domain, $localedir);
+textdomain($domain);
+
+echo gettext('hello-world') . '<br/>';
+?>
